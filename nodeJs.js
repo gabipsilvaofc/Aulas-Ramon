aa53561c76ce8280973a87360e65c50aeeb0e5cf
Gabriela Silva <163335365+gabipsilvaofc@users.noreply.github.com>
2024-08-05 14:00:01 -0300

    retorno api ramon

--- nodeJs.js
+++ nodeJs.js
@@ -1,17 +1,36 @@
+// const express = require('express')
+// const app = express()
+// const bodyParser = require('body-parser')
+// const palindromo = require('./testeJS')
+
+// app.use(bodyParser.json())
+
+// app.post('/e-palindromo', function (req, res){
+//     const resposta = palindromo(req.body.palavra)
+//     if(resposta === true){
+//         res.send(true)
+//         }
+//         res.send(false)
+//     }
+// )
+
+// app.listen(8000)
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const palindromo = require('./testeJS')
 
 app.use(bodyParser.json())
 
-app.post('/e-palindromo', function (req, res){
-    const resposta = palindromo(req.body.palavra)
-    if(resposta === true){
-        res.send(true)
-        }
-        res.send(false)
-    }
-)
+const db = require('./db.json')
+app.get('/produtos', function (req,res){
+    var produtos = db.produtos
+    res.json(produtos)
+})
+
+app.get('/produtos/:id', function (req,res){
+    const _id = req.params.id
+    res.send(_id)
+})
 
 app.listen(8000)
